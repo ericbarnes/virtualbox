593dc0dcf7116aa150a14e619a21bdc6420df3a7
Archie Gunasekara <achintha@me.com>
2015-09-04 15:29:27 +1000

    Now the script is printing the progress Also hardcoded configuration was moved to the configuration section on the top

--- serve.sh
+++ serve.sh
@@ -1,4 +1,17 @@
+print_info () {
+
+	local s=$1
+ 
+	echo "#################################################"
+	echo "$1"
+	echo "#################################################"
+}
+
+print_info "Updating the hosts file"
+
 echo "127.0.0.1 $1" >> "/etc/hosts"
+
+print_info "Updating the Apache Configuration"
 
 vhost="<VirtualHost *:80>
   ServerName $1
@@ -14,5 +27,7 @@ vhost="<VirtualHost *:80>
 echo "$vhost" >> "/etc/apache2/sites-available/$1.conf"
 
 ln -s "/etc/apache2/sites-available/$1.conf" "/etc/apache2/sites-enabled/$1.conf"
+
+print_info "Restarting Apache"
 
 /etc/init.d/apache2 reload
